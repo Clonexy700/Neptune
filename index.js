b93c231b4dad71778690fd7de89d8abfd1812b43
Clonexy700 <55646391+Clonexy700@users.noreply.github.com>
2020-08-08 19:43:17 +0700

    information execute

--- index.js
+++ index.js
@@ -90,12 +90,26 @@ client.on('message', (msg) => {
         msg.channel.send(embed)
     }
 
+    if (msg.content === (prefix + 'info')) {
+        var finalString = '';
+        var user = msg.author;
+        var userCreated = user.createdAt.toString().split(' ');
+        const embed = new Discord.MessageEmbed()
+        .addField(`${user.username}'s information`, `${user.tag}`)
+        .addField(`Presence`, `${user.presence.status}`)
+        .addField('Account created:\n ', `${userCreated[1]}, ${userCreated[2]}d  ${userCreated[3]}y, ${userCreated[4]}`)
+        .setFooter(`ID: ${user.id}\n ｡^‿^｡`)
+        .setTimestamp(msg.createdAt)
+        .setColor('#5d5ddf')
+        msg.channel.send(embed)
+    }
+
     if (msg.content === (prefix + 'alicard')) {
         const embed  = new Discord.MessageEmbed()
         .addField('Nep nep!', '``He is perv and lewding lolies >.<``', false)
         .setColor('#f53127')
         .setImage('https://cdn.discordapp.com/attachments/607250459622768640/740811878418219100/satan.png')
-        msg.channel.send(embed)
+        msg.channel .send(embed)
     }
 
     if (msg.content === (prefix + 'ping')) {
