a5b892e77c05c5ad55ec53acef3b1ff238d34a89
Clonexy700 <55646391+Clonexy700@users.noreply.github.com>
2020-08-13 18:32:12 +0700

    lmgtfy little changes

--- commands/lmgtfy.js
+++ commands/lmgtfy.js
@@ -3,7 +3,13 @@ const encode = require('strict-uri-encode')
 module.exports.run = async(client, msg, args) => {
     let question = encode(args.join(" "));
     let link = `https://www.lmgtfy.com/?q=${question}`
-    msg.channel.send(`**<${link}>`)
+    let embed = new Discord.MessageEmbed()
+    .setColor("#00FF00")
+    .setTimestamp()
+    .setFooter(client.user.username, client.user.displayAvatarURL())
+    .setTitle("lmgtfy, nep")
+    .setDescription(`**<${link}>**`)
+    msg.channel.send(embed);
 }
 module.exports.config = {
     command: "lmgtfy"
