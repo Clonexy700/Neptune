262a16282744eef2624a3ade40c9316aab53550a
Clonexy700 <55646391+Clonexy700@users.noreply.github.com>
2020-08-13 12:31:58 +0700

    weather to handler move

---
+++ commands/weather.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js');
+const fs = require('fs');
+const weather = require('weather-js')
+module.exports.run = async(client, msg, args) => {
+    weather.find({search: args.join(" "), degreeType: 'C'}, function(err, result) {
+        if (err) msg.channel.send(err);
+
+        var current = result[0].current;
+        var location = result[0].location;
+
+        const embed = new Discord.MessageEmbed()
+        .setDescription(`\`\`${current.skytext}\`\``)
+        .setAuthor(`Weather for ${current.observationpoint}`)
+        .setThumbnail(current.imageUrl)
+        .setColor('#fed9f3')
+        .addField('Timezone', `UTC: +${location.timezone}`, true)
+        .addField('Degree type', `${location.degreetype}`, true)
+        .addField('Temperature', `${current.temperature}`, true)
+        .addField('Feels like', `${current.feelslike}`, true)
+        .addField('Winds', `${current.feelslike}`, true)
+        .addField('Feels like', `${current.winddisplay}`, true)
+        .addField('Humidity', `${current.humidity}%`, true)
+
+        msg.channel.send(embed)
+    });
+}
+
+module.exports.config = {
+    command: "weather"
+}
--- index.js
+++ index.js
@@ -307,30 +307,6 @@ client.on('message', (msg) => {
 
     }
 
-    if (msg.content.startsWith(prefix + 'weather')) {
-        weather.find({search: args.join(" "), degreeType: 'C'}, function(err, result) {
-            if (err) msg.channel.send(err);
-
-            var current = result[0].current;
-            var location = result[0].location;
-
-            const embed = new Discord.MessageEmbed()
-            .setDescription(`\`\`${current.skytext}\`\``)
-            .setAuthor(`Weather for ${current.observationpoint}`)
-            .setThumbnail(current.imageUrl)
-            .setColor('#fed9f3')
-            .addField('Timezone', `UTC: +${location.timezone}`, true)
-            .addField('Degree type', `${location.degreetype}`, true)
-            .addField('Temperature', `${current.temperature}`, true)
-            .addField('Feels like', `${current.feelslike}`, true)
-            .addField('Winds', `${current.feelslike}`, true)
-            .addField('Feels like', `${current.winddisplay}`, true)
-            .addField('Humidity', `${current.humidity}%`, true)
-
-            msg.channel.send(embed)
-        });
-    }
-
 });
 
 client.login(token);
