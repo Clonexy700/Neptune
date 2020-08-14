a93e64f98ab0130b7eaa0c500cf44fb398ff121c
Clonexy700 <55646391+Clonexy700@users.noreply.github.com>
2020-08-14 17:03:08 +0700

    Update carrot.js

--- commands/carrot.js
+++ commands/carrot.js
@@ -1,9 +1,10 @@
 const Discord = require('discord.js');
 const fs = require('fs');
 module.exports.run = async(client, msg, args) => {
-
+    msg.delete();
     const embed = new Discord.MessageEmbed()
     .setColor('#fed9f3')
+    .setFooter(msg.author.username, msg.author.displayAvatarURL())
     .setImage('https://cdn.discordapp.com/attachments/581531385639206935/740827197132242954/3099b51e40f0f8fd.gif')
     msg.channel.send(embed)
 
