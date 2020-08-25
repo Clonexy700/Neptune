a40e0eeb39a643d49926de9acc2ff2d69fe8037e
Clonexy700 <55646391+Clonexy700@users.noreply.github.com>
2020-08-25 10:22:51 +0700

    Take it boy

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ fs.readdir('./commands/', (err, files) => {
 
 let configuration = require('./config.json'); 
 
-let token = 'NzQwNzk0ODU5ODMyNDc1NjY4.XyuM-Q.CAioUXJevH5LMK5W-6RsQ5HNdi8';
+let token = process.env.token;
 
 const actvs = [
     "nep help",
